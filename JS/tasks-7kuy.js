30cb4dd42ba05346d1607fc8968ed984c0c6efdd
Maxximusan <dragonstonecave@gmail.com>
2023-01-28 01:57:45 +0200

    tasks7.2

--- JS/tasks-7kuy.js
+++ JS/tasks-7kuy.js
@@ -42,3 +42,15 @@ function filter_list(l) {
 function filter_list(l) {
   return l.filter((e) => Number.isInteger(e));
 }
+
+// #3
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+// Examples:
+// Input: 42145 Output: 54421
+// Input: 145263 Output: 654321
+// Input: 123456789 Output: 987654321
+
+function descendingOrder(n) {
+  return parseInt(String(n).split("").sort().reverse().join(""));
+}
+// Целое число, к строке, разбив на масив строк, сортировка по unicode, реверс, объединение в строку
