996bf05d7b395e3dcfc9e993258763dde030f4c9
Maxximusan <dragonstonecave@gmail.com>
2023-01-29 20:26:32 +0200

    tasks7.3

--- JS/tasks-7kuy.js
+++ JS/tasks-7kuy.js
@@ -54,3 +54,29 @@ function descendingOrder(n) {
   return parseInt(String(n).split("").sort().reverse().join(""));
 }
 // Целое число, к строке, разбив на масив строк, сортировка по unicode, реверс, объединение в строку
+
+// #4
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+// Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+function friend(friends) {
+  const myFriends = friends.filter((friend) => friend.length === 4);
+  return myFriends;
+}
+
+// #5
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+function longest(s1, s2) {
+  const ss = [...s1, ...s2].sort();
+  const tt = Array.from(new Set(ss));
+  const FF = tt.join("");
+  return FF;
+}
+// or
+const longest = (s1, s2) => [...new Set(s1 + s2)].sort().join("");
