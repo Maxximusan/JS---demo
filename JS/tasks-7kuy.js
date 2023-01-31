41be81db2645d3a0324c7678365db6094d5ad425
Maxximusan <dragonstonecave@gmail.com>
2023-01-31 23:18:23 +0200

    tasks7.4

--- JS/tasks-7kuy.js
+++ JS/tasks-7kuy.js
@@ -80,3 +80,22 @@ function longest(s1, s2) {
 }
 // or
 const longest = (s1, s2) => [...new Set(s1 + s2)].sort().join("");
+
+// #6
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+// Example: (Input --> Output)
+// "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+// isIsogram "Dermatoglyphics" = true
+// isIsogram "moose" = false
+// isIsogram "aba" = false
+function isIsogram(str) {
+  const set = new Set(str.toUpperCase()).size;
+  // console.log(str.length === set);
+  if (str.length === set) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+isIsogram("qwerty");
