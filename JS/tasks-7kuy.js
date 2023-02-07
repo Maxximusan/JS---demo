98a10688aa2e9327e663f4dffcf201a0bf512e8d
Maxximusan <dragonstonecave@gmail.com>
2023-02-07 23:13:48 +0200

    tasks7.6

--- JS/tasks-7kuy.js
+++ JS/tasks-7kuy.js
@@ -142,3 +142,23 @@ function bubblesortOnce(a) {
     e > arr[i + 1] ? ((arr[i] = arr[i + 1]), (arr[i + 1] = e), arr[i]) : e
   );
 }
+
+// #8
+// Sum of two lowest positive integers
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+function sumTwoSmallestNumbers(numbers) {
+  var [lowest1, lowest2, ...others] = numbers.sort(
+    (currentEl, nextEl) => currentEl - nextEl
+  );
+  return lowest1 + lowest2;
+}
+
+// or
+
+function sumTwoSmallestNumbers(numbers) {
+  numbers.sort((a, b) => a - b);
+  return numbers[0] + numbers[1];
+}
