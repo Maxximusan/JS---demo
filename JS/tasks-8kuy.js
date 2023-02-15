b25f0ce5b5d16476ef0cdfed07338c5760848abd
Maxximusan <dragonstonecave@gmail.com>
2023-02-15 21:06:42 +0200

    tasks8.2

--- JS/tasks-8kuy.js
+++ JS/tasks-8kuy.js
@@ -175,3 +175,17 @@ function solution(str, ending) {
   return str.slice(0 - ending.length) === ending;
 }
 
+// #8
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+// "hello world".toAlternatingCase() === "HELLO WORLD";
+// "HELLO WORLD".toAlternatingCase() === "hello world";
+// "hello WORLD".toAlternatingCase() === "HELLO world";
+// "HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld";
+String.prototype.toAlternatingCase = function () {
+  return this.split("")
+    .map((el) =>
+      el === el.toLowerCase() ? el.toUpperCase() : el.toLowerCase()
+    )
+    .join("");
+};
